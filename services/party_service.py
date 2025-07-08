5aa26b5f0e75a6222edbb56c9e648f2a53a3d7b3
alaz-linux <alex.lazaris617@gmail.com>
2025-07-08 22:48:00 +1000

    feat: refactor how to add parties & validations involved

--- services/party_service.py
+++ services/party_service.py
@@ -5,28 +5,29 @@ from models import Party, Save
 
 
 def create_party(body, id):
-    Save.query.get_or_404(id)
+    target_save = Save.query.get(id)
+    if target_save == None:
+        abort(404, message=f"Save {id} cannot be found. Ensure the save id valid.")
+    # step 1 - form new party
     new_party = []
     for p in body:
         member = Party(name=p["name"], save_id=id)
         new_party.append(member)
-    new_party_length = new_party.__len__()
-    if new_party_length < 1 or new_party_length > 3:
-        abort(400, message="Party size invalid. Must between 1-3 members.")
+    # step 2 - validate new party
+    if new_party.__len__() < 1 or new_party.__len__() > 3:
+        abort(400, message="Party size invalid. Must be between 1-3 members.")
     try:
         db.session.add_all(new_party)
         db.session.commit()
-    # TODO: double check if this validation is right, is line 8 needed?
+        return new_party
     except IntegrityError as e:
         abort(
             400,
-            message=f"A party already exists for this save. Instead, edit the party to modify your party members.",
+            message=f"Error adding new members. Ensure each party member is unique.",
         )
     except SQLAlchemyError():
         db.session.rollback()
         abort(500, message="Error occurred whilst inserting record.")
-    return new_party
-
 
 def get_party_using_save(id):
     try:
@@ -38,11 +39,14 @@ def get_party_using_save(id):
         abort(500, message="Something went wrong.")
 
 
+# TODO: this delete->add->save approach is dirty, makes too many table changes. Need to improve this. Perhaps straight swap the party member's name instead?
 def update_party_using_save(body, id):
-    # add step to validate incoming members
-    # must be within 1-3 members - DONE
-    # must be unique - TODO
     try:
+        # step 1 - validate target save
+        target_save = Save.query.get(id)
+        if target_save == None:
+            abort(404, message=f"Save {id} cannot be found. Ensure the save id valid.")
+        # step 2 - delete existing party
         current_members = []
         party = Party.query.filter_by(save_id=id).all()
         for p in party:
@@ -50,23 +54,26 @@ def update_party_using_save(body, id):
         for delete in party:
             db.session.delete(delete)
         db.session.commit()
-        new_members = []
-        for m in body:
-            new_members.append(m["name"])
-        return create_party(body, id)
+        # step 3 - form new party
+        new_party = []
+        for p in body:
+            member = Party(name=p["name"], save_id=id)
+            new_party.append(member)
+        # step 4 - validate new party
+        if new_party.__len__() < 1 or new_party.__len__() > 3:
+            abort(400, message="Party size invalid. Must be between 1-3 members.")
+        try:
+            db.session.add_all(new_party)
+            db.session.commit()
+            return new_party
+        except IntegrityError as e:
+            abort(
+                400,
+                message=f"Error adding new members. Ensure each party member is unique.",
+            )
+        except SQLAlchemyError():
+            db.session.rollback()
+            abort(500, message="Error occurred whilst updating record.")
     except SQLAlchemyError as e:
         db.session.rollback()
         abort(500, messcreate_partyage=f"Error updating party: {e._sql_message}")
-
-
-def delete_party_using_save(id):
-    try:
-        party = Party.query.filter_by(save_id=id).all()
-        count = len(party)
-        for delete in party:
-            db.session.delete(delete)
-        db.session.commit()
-        return {"message": f"deleted {count} party member/s"}
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        abort(500, messcreate_partyage=f"Error deleting party: {e._sql_message}")
