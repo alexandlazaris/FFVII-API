ba10ee832c30a820c772470bc8eac79f27359510
alexandlazaris <alex.lazaris617@gmail.com>
2025-11-03 21:17:10 +1100

    feat: query params schema for GET /materia

--- schemas/materia.py
+++ schemas/materia.py
@@ -1,7 +1,13 @@
 from marshmallow import Schema, fields
 
+
 class MateriaSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     element = fields.Str(required=True)
     type = fields.Str(required=True)
+
+
+class GetMateriaSchemaQueries(Schema):
+    type = fields.Str(required=False)
+    element = fields.Str(required=False)
