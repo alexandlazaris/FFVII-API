624bace50de2720c236a50bd0c9de64d8f26968e
alexandlazaris <alex.lazaris617@gmail.com>
2025-11-03 21:18:41 +1100

    test: manually seed data into db for 1 test & add extra assertions

--- tests/unit/test_materia.py
+++ tests/unit/test_materia.py
@@ -1,20 +1,58 @@
-def test_create_materia_data(client):
-    response = client.post("/materia/data")
-    assert response.status_code == 201
+from models import MateriaModel
+from app import db
 
 
 def test_create_new_materia(client):
-    body = [{"name": "bolt", "element": "lightning", "level": 1}]
+    body = [
+        {"name": "Comet2", "type": "magic", "element": "non-elemental"},
+    ]
     response = client.post("/materia", json=body)
     assert response.status_code == 201
+    json = response.get_json()
+    assert json[0]["name"] == body[0]["name"]
+    assert json[0]["type"] == body[0]["type"]
+    assert json[0]["element"] == body[0]["element"]
 
 
-def test_get_all_materia(client):
-    body = [
-        {"name": "bolt", "element": "lightning", "level": 1},
-        {"name": "bio", "element": "poison", "level": 2},
+def test_get_materia_using_filter_type(client, app):
+    data = [
+        {"id": 1, "name": "Comet", "type": "magic", "element": "non-elemental"},
+        {"id": 2, "name": "Barrier", "type": "magic", "element": None},
+        {"id": 3, "name": "Time", "type": "magic", "element": "time"},
+        {"id": 4, "name": "test_name", "type": "test_type", "element": "test_element"},
     ]
-    client.post("/materia", json=body)
 
-    response = client.get("/materia")
+    with app.app_context():
+        for d in data:
+            new_materia = MateriaModel(**d)
+            db.session.add(new_materia)
+        db.session.commit()
+
+    response = client.get("/materia?type=test_type")
     assert response.status_code == 200
+    json = response.get_json()
+    assert json[0]["name"] == data[3]["name"]
+    assert json[0]["type"] == data[3]["type"]
+    assert json[0]["element"] == data[3]["element"]
+
+
+def test_get_materia_using_filter_element(client, app):
+    data = [
+        {"id": 1, "name": "Comet", "type": "magic", "element": "non-elemental"},
+        {"id": 2, "name": "Barrier", "type": "magic", "element": None},
+        {"id": 3, "name": "Time", "type": "magic", "element": "time"},
+        {"id": 4, "name": "test_name", "type": "test_type", "element": "test_element"},
+    ]
+
+    with app.app_context():
+        for d in data:
+            new_materia = MateriaModel(**d)
+            db.session.add(new_materia)
+        db.session.commit()
+
+    response = client.get("/materia?element=test_element")
+    assert response.status_code == 200
+    json = response.get_json()
+    assert json[0]["name"] == data[3]["name"]
+    assert json[0]["type"] == data[3]["type"]
+    assert json[0]["element"] == data[3]["element"]
