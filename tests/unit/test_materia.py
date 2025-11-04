8f234002c34f620fe150db3eb0adbe9b333de8a9
alexandlazaris <alex.lazaris617@gmail.com>
2025-11-04 20:22:05 +1100

    test: add test for DELETE /materia

--- tests/unit/test_materia.py
+++ tests/unit/test_materia.py
@@ -87,6 +87,26 @@ def test_get_materia_using_filter_desc(client, app):
     json = response.get_json()
     first_materia = json[0]
     last_materia = json[len(json)-1]
-    print (json)
     assert first_materia == data[4]
     assert last_materia == data[3]
+
+def test_delete_all_materia(client, app):
+    data = [
+        {"id": "1", "name": "Comet", "type": "magic", "element": "non-elemental"},
+        {"id": "2", "name": "Barrier", "type": "magic", "element": None},
+        {"id": "3", "name": "Time", "type": "magic", "element": "time"},
+        {"id": "4", "name": "A_test_name", "type": "test_type", "element": "test_element"},
+        {"id": "5", "name": "Z_test_name", "type": "test_type", "element": "test_element"},
+    ]
+
+    with app.app_context():
+        for d in data:
+            new_materia = MateriaModel(**d)
+            db.session.add(new_materia)
+        db.session.commit()
+
+    response = client.delete("/materia")
+    assert response.status_code == 200
+    json = response.get_json()
+    expected_message = f"deleted {len(data)} materia"
+    assert json["message"] == expected_message
