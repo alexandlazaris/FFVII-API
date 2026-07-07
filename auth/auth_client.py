aef3778724474f895c55c66acab92873f2176bff
Alaz <alex.lazaris617@gmail.com>
2026-07-07 21:27:24 +1000

    feat: auth client class

---
+++ auth/auth_client.py
@@ -0,0 +1,34 @@
+import os
+import logging
+from supabase import create_client, Client
+
+logger = logging.getLogger(__name__)
+
+
+class AuthClient:
+    def __init__(self):
+        self.url: str | None = os.getenv("SUPABASE_URL")
+        self.key: str | None = os.getenv("SUPABASE_KEY")
+        self.auth_client: Client
+
+    def client_setup(self):
+        self.auth_client: Client = create_client(
+            supabase_url=self.url or "", supabase_key=self.key or ""
+        )
+        logging.info("auth client created")
+
+    def create_dummy_user(self):
+        response = self.auth_client.auth.sign_in_anonymously(
+            {"options": {"data": {"name": "anon_test_user"}}}
+        )
+        return response
+
+    def login(self, username: str, password: str):
+        self.auth_client.auth.sign_in_with_password(
+            {"email": username, "password": password}
+        )
+
+    def signup(self, email: str, password: str):
+        return self.auth_client.auth.sign_up({"email": email, "password": password})
+
+auth = AuthClient()
