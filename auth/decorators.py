d4608f7e2523f20f697cfac6c5c9a81a44630262
Alaz <alex.lazaris617@gmail.com>
2026-07-24 22:16:27 +1000

    feat: introduce jwt verification & decorator

---
+++ auth/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from flask import g, request
+from auth.verifier import verify_access_token
+from auth.exceptions import InvalidTokenError
+
+def require_auth(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header:
+            raise InvalidTokenError("Missing authorization header")
+
+        if not auth_header.startswith("Bearer "):
+            raise InvalidTokenError("Invalid authorization header")
+
+        token = auth_header.removeprefix("Bearer ").strip()
+        user = verify_access_token(token)
+        g.user = user
+        return func(*args, **kwargs)
+    return wrapper
