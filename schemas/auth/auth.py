6f83976c7d8d6f533dea60dcd12b88e706bbf854
Alaz <alex.lazaris617@gmail.com>
2026-07-24 22:17:31 +1000

    feat: auth service in progress, schemas added, needs tidying up

--- schemas/auth/auth.py
+++ schemas/auth/auth.py
@@ -6,3 +6,15 @@ class SignUpRequestSchema(Schema):
 
 class SignUpResponseSchema(Schema):
     email = fields.Email(dumpOnly=True)
+    email_verified = fields.Str(dumpOnly=True)
+
+class LoginWithPasswordRequestSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+
+class LoginWithPasswordResponseSchema(Schema):
+    access_token = fields.Str(dump_only=True)
+    refresh_token = fields.Str(dump_only=True)
+
+class LoginWithPasswordErrorSchema(Schema):
+    error = fields.Str(dump_only=True)
--- services/auth/auth_service.py
+++ services/auth/auth_service.py
@@ -1,8 +1,9 @@
 from flask_smorest import abort
 from pydantic import ValidationError
+from supabase import AuthApiError
 from db import db
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-from models.auth.auth import Signup
+from models.auth.auth import LoginResponse, SignupRequest, LoginRequest, SignUpResponse
 import json
 from flask import jsonify
 import logging
@@ -10,18 +11,64 @@ from auth.auth_client import auth
 
 logger = logging.getLogger(__name__)
 
+class EmailExistsError(Exception):
+    pass
 
-def signup_with_email_password(body):
+class EmailRateLimitExceededError(Exception):
+    pass
+
+class EmailNotConfirmedError(Exception):
+    pass
+
+class InvalidCredentialsError(Exception):
+    pass
+
+
+def signup_with_email_password(email: str, password: str) -> SignUpResponse:
     try:
-        logger.info("validating new user")
-        request_validated = Signup.model_validate(body)
-        # TODO: make a model for the return sign up response
-        response = auth.signup(request_validated.email, request_validated.password)
-    except ValidationError:
-        abort(400, message="Invalid request. Validation failed for email/password")
-    return response
+        response = auth.signup(email, password)
+        print (response, flush=True)
+        if response.session is None:
+            raise RuntimeError("Expected session from email signup, returned Exception")
+        return SignUpResponse(
+            email=response.session.user.user_metadata["email"],
+            email_verified=response.session.user.user_metadata["email_verified"],
+        )
+    except AuthApiError as e:
+        match e.code:
+            case "email_exists":
+                raise EmailExistsError()
+            case "over_email_send_rate_limit":
+                raise EmailRateLimitExceededError()
+            case _:
+                raise
 
-# TODO:
-# make a login function
+def get_error_metadata(e:AuthApiError):
+    print (e, flush=True)
+    print (e.message, flush=True)
+    print (e.code, flush=True)
+    print (e.status, flush=True)
+
+def login_with_password(email: str, password: str) -> LoginResponse:
+    try:
+        response = auth.login(email, password)
+        if response.session is None:
+            raise RuntimeError(
+                "Expected session from password login, returned Exception"
+            )
+        return LoginResponse(
+            access_token=response.session.access_token,
+            refresh_token=response.session.refresh_token,
+        )
+    except AuthApiError as e:
+        match e.code:
+            case "email_not_confirmed":
+                raise EmailNotConfirmedError()
+            case "invalid_credentials":
+                raise InvalidCredentialsError()
+            case _:
+                raise
+
+
 # make a logout function
 # make a delete account function
