e5abf93df1e360d01f9f1cd8e489a65c570a5c7d
Alaz <alex.lazaris617@gmail.com>
2026-08-27 09:33:53 +1000

    missed the schema for saves

--- schemas/saves.py
+++ schemas/saves.py
@@ -1,8 +1,12 @@
+from typing import Optional
 from pydantic import BaseModel
 from marshmallow import Schema, fields
+from uuid import UUID
+
 
 class SaveRequestSchema(Schema):
     location = fields.Str(required=True)
+
 
 class SaveResponseSchema(Schema):
     id = fields.Str(dump_only=True)
@@ -10,11 +14,23 @@ class SaveResponseSchema(Schema):
     party = fields.List(fields.Str(dump_only=True))
     party_lead = fields.Dict(dump_only=True)
 
+
 class SaveItemCreateRequest(BaseModel):
     location: str
     disc: int = 1
 
+
 class SaveItemCreateResponse(BaseModel):
-    location: str 
+    location: str
     id: str
     disc: int
+    user_id: UUID
+
+
+class GetSaveItemResponse(BaseModel):
+    location: str
+    id: str
+    disc: int
+    user_id: UUID
+    party_lead: Optional[str] = None
+    party: Optional[list[str]] = None
